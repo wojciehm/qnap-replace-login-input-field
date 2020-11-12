f6ebad84d8e037c53ba1cee9c3e3c6c08cef469d
Wojciech Marusiak <wojcieh@gmail.com>
2020-11-12 14:41:56 +0100

    Update replace-login.sh

--- replace-login.sh
+++ replace-login.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# this script is to convert automatically a folder of video files to mp4
-
-source=/home/httpd/cgi-bin/login.html
-destination=/root/loginbackup
-cp -a -- "$source" "/$destination/${source##*/}-$(date +"%m-%d-%y-%r")" 2>&1 | tee /root/loginbackup/log$(date +"%m-%d-%y-%r").log
-sed 's/textarea class/input class/' $source > $source 2>&1 | tee /root/loginbackup/log$(date +"%m-%d-%y-%r").log
+# this script will make a backup of your QNAP login file located in /root/loginbackup/login.html and replace input field
+cp /home/httpd/cgi-bin/login.html "/root/loginbackup/login.html-$(date +"%m-%d-%y-%r")"
+sed 's/textarea class/input class/' /home/httpd/cgi-bin/login.html
