190397e03f0af00ac67b4d9b817e7682c09f6515
Wojciech Marusiak <wojcieh@gmail.com>
2022-10-03 12:39:27 +0200

    Added directory prior backup

--- replace-login.sh
+++ replace-login.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # this script will make a backup of your QNAP login file located in /root/loginbackup/login.html and replace input field
+mkdir /root/loginbackup/
 cp /home/httpd/cgi-bin/login.html "/root/loginbackup/login.html-$(date +"%m-%d-%y-%r")"
 sed -i 's/textarea class/input class/' /home/httpd/cgi-bin/login.html
 /etc/init.d/Qthttpd.sh restart
